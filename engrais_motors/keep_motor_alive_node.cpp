2e2bd643c4338a0d33b6c3483c74ad6a6f6a708d
Remy Guyonneau <remy.guyonneau@univ-angers.fr>
2019-11-19 12:53:33 +0100

    modifications after ez wheel

--- engrais_motors/keep_motor_alive_node.cpp
+++ engrais_motors/keep_motor_alive_node.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv){
     std::string port_name_1 = "/dev/ttyUSB1";
     int baud = 57600;
     //node.getParam("baud", baud);
-    int timeout = 10;
+    int timeout = 50;
     //node.getParam("timeout", timeout);
     int bytesize = 8;
     //node.getParam("data_bits", bytesize);
@@ -81,49 +81,52 @@ int main(int argc, char **argv){
     tramme_1[ 0] = 0xAA;
     tramme_1[ 1] = 0x0F;
     tramme_1[ 2] = 0x08;
-    tramme_1[ 3] = 0x00 + cont; 
-    tramme_1[ 4] = 0x00;
+    tramme_1[ 3] = 0x10 + cont; 
+    tramme_1[ 4] = 0x01;
     tramme_1[ 5] = 0x0A;
     tramme_1[ 6] = 0x00;
     tramme_1[ 7] = 0x00;
     tramme_1[ 8] = 0x00;
     tramme_1[ 9] = 0x01;
     tramme_1[10] = 0x05;
-    tramme_1[11] = 0x01;
+
+    tramme_1[11] = 0x00;
     tramme_1[12] = 0x00;
-    tramme_1[13] = 0x09;
-    tramme_1[14] = 0x0A;
+    tramme_1[13] = 0x20;
+    tramme_1[14] = 0x02;
+
     tramme_1[15] = 0x04;
     addCRC(tramme_1, tramme_1_size);
 
 
-    size_t tramme_2_size = 22;
+    size_t tramme_2_size = 21;
     uint8_t tramme_2[tramme_1_size];
 
     tramme_2[ 0] = 0xAA;
-    tramme_2[ 1] = 0x14;
+    tramme_2[ 1] = 0x13;
     tramme_2[ 2] = 0x08;
-    tramme_2[ 3] = 0x00 + cont;
+    tramme_2[ 3] = 0x10 + cont;
     tramme_2[ 4] = 0x00;
-    tramme_2[ 5] = 0x0F;
+    tramme_2[ 5] = 0x0E;
     tramme_2[ 6] = 0x00;
     tramme_2[ 7] = 0x00;
     tramme_2[ 8] = 0x00;
-    tramme_2[ 9] = 0x03;
+    tramme_2[ 9] = 0x02;
     tramme_2[10] = 0x05;
     tramme_2[11] = 0x00;
     tramme_2[12] = 0x00;
     tramme_2[13] = 0x04;
     tramme_2[14] = 0x07;
     tramme_2[15] = 0x04;
-    tramme_2[16] = 0x05;
+
+    tramme_2[16] = 0x16;
     tramme_2[17] = 0x00;
-    tramme_2[18] = 0x00;
+    tramme_2[18] = 0x0A * 2;
     tramme_2[19] = 0x00;
-    tramme_2[20] = 0x00;
+
     addCRC(tramme_2, tramme_2_size);
 
-    size_t sniff_size = 200;
+    size_t sniff_size = 13;
     uint8_t sniff[sniff_size];
     int response = 0;
 
@@ -131,15 +134,15 @@ int main(int argc, char **argv){
     com.fill('0');
     arq.fill('0');
 
-    for(int i = 0; i < 40; i++){
+    /*for(int i = 0; i < 40; i++){
 	    my_serial_1.write(tramme_1, tramme_1_size);
 	    ros::Duration(0.05).sleep();
-	}
+	}*/
 
     int vel = 0;
 
-    tramme_2[18] = 0x00;
-    addCRC(tramme_2, tramme_2_size);
+    /*tramme_2[18] = 0x00;
+    addCRC(tramme_2, tramme_2_size);*/
 
     int requestCont = 1;
 
@@ -147,7 +150,7 @@ int main(int argc, char **argv){
 
 	    my_serial_1.write(tramme_1, tramme_1_size);
 
-	    ros::Duration(0.05).sleep();
+	    //ros::Duration(0.05).sleep();
 
 	    cout << "Send Question " << requestCont << ": " << endl;
 	    com << "Send Question " << requestCont << ": " << endl;
@@ -183,8 +186,10 @@ int main(int argc, char **argv){
 
 	    ros::Duration(0.01).sleep();
 
-	    tramme_2[3] = ++cont;
-        //tramme_2[18] = vel/10 >= 0x1E ? 0x1E : (++vel)/10;
+        /*if(++cont == 0x10){
+            cont = 0;
+        }
+	    tramme_2[3] = 0x10 + cont;
         addCRC(tramme_2, tramme_2_size);
 
 
@@ -219,14 +224,21 @@ int main(int argc, char **argv){
 	    }
 
 	    cout << endl << endl;
-	    com << endl << endl;
+	    com << endl << endl;*/
+        
+        if(++cont == 0x10){
+            cont = 0;
+        }
 
-	    ros::Duration(0.01).sleep();
-
-	    tramme_1[3] = ++cont;
+        tramme_1[3] = 0x10 + cont;
         addCRC(tramme_1, tramme_1_size);
 
-        requestCont++;
+        /*tramme_1[3] = 0x10 + cont;
+        addCRC(tramme_1, tramme_1_size);*/
+
+
+        /*requestCont++;*/
+        ros::Duration(0.01).sleep();
 
     }
 
