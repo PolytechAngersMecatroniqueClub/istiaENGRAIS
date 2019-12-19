7a1792734fc057565aa0d19004200e6f5199610c
Gabriel Oliveira <gftsoliveira@gmail.com>
2019-12-19 16:57:45 +0100

    update scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,34 +1,34 @@
 mkdir -p ~/temp
 
-sudo apt install git
+yes | sudo apt install git
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt update
-sudo apt install ros-melodic-desktop-full
+yes | sudo apt update
+yes | sudo apt install ros-melodic-desktop-full
 
 apt search ros-melodic
 
-sudo rosdep init
+yes | sudo rosdep init
 rosdep update
 
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
-sudo apt-get install unzip
+yes | sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+yes | sudo apt-get install unzip
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
 
-sudo apt-get update
-sudo apt-get install gazebo9
-sudo apt-get install libgazebo9-dev
-sudo apt upgrade libignition-math2
+yes | sudo apt-get update
+yes | sudo apt-get install gazebo9
+yes | sudo apt-get install libgazebo9-dev
+yes | sudo apt upgrade libignition-math2
 
-sudo apt install ros-melodic-gazebo-ros
-sudo apt install ros-melodic-controller-manager
-sudo apt install ros-melodic-velocity-controllers
-sudo apt install ros-melodic-controller-interface
-sudo apt install ros-melodic-joint-state-controller
-sudo apt install ros-melodic-effort-controllers
+yes | sudo apt install ros-melodic-gazebo-ros
+yes | sudo apt install ros-melodic-controller-manager
+yes | sudo apt install ros-melodic-velocity-controllers
+yes | sudo apt install ros-melodic-controller-interface
+yes | sudo apt install ros-melodic-joint-state-controller
+yes | sudo apt install ros-melodic-effort-controllers
 
 cd ~/temp/
 git clone https://github.com/PolytechAngersMecatroniqueClub/istiaENGRAIS.git
@@ -69,4 +69,4 @@ printf "\nsource ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 
 rm -rf ~/temp
 
-source ~/catkin_ws/devel/setup.bash
+echo "Done"
